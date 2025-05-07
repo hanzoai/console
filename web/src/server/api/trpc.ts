490e6e329fe51ab967f5f303f2ac393ae9ed98e9
PCMAC <pcmac@Mac-mini-cua-PCMAC.local>
2025-05-07 12:51:42 +0700

    LOGGIN

--- web/src/server/api/trpc.ts
+++ web/src/server/api/trpc.ts
@@ -295,6 +295,7 @@ const enforceIsAuthedAndOrgMember = t.middleware(({ ctx, rawInput, next }) => {
   const sessionOrg = ctx.session.user.organizations.find(
     (org) => org.id === orgId,
   );
+  logger.error(`Check org session:>>>>`, sessionOrg);
 
   if (
     !sessionOrg
